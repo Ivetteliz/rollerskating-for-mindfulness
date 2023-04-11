7c97924bd7e0acb58f2e02d7fc7d15e215eb9123
Ivetteliz <111955246+Ivetteliz@users.noreply.github.com>
2023-04-10 18:30:38 -0700

    Created some functions for stereo
    -Created variables targeting the ID's needed
    - Made a variable for song title with an array with songs in music folder
    - variable 'songIndex' is to keep track of song playing in the array
    -'loadSong' will now function when play button is clicked due to the event listener 'playBtn' as well as making the vinyl disc rotate 360 deg
    -working towards making the function for pause button to appear when play is clicked

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,34 @@
+const musicContainer = document.querySelector("#music-container");
+const playBtn = document.querySelector("#play");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+const audio = document.querySelector("#audio");
+const progress = document.querySelector("#progress");
+const progressContainer = document.querySelector("#progress-container");
+const title = document.querySelector("#title");
 
+let song = ["Melting", "Good Days", "Chamber of Reflection"];
+let songIndex = 0;
+
+loadSong(song[songIndex]);
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+}
+
+function playSong() {
+  musicContainer.classList.add("play");
+}
+
+function pauseSong() {}
+
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
